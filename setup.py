8b779731264cbac5a00c7a24cc0ed6e01d495b5e
Mr-Moeen <156661459+Mr-Moeen@users.noreply.github.com>
2024-04-13 12:27:06 +0500

    setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,12 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-
 __version__ = "0.0.0"
 
-REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "Text-summerization-NLP"
+AUTHOR_USER_NAME = "Mr-Moeen"
 SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
-
-
+AUTHOR_EMAIL = "abdulmoeen.mm789@gmail.com"
 
 setuptools.setup(
     name=SRC_REPO,
@@ -20,7 +17,7 @@ setuptools.setup(
     author_email=AUTHOR_EMAIL,
     description="A small python package for NLP app",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
